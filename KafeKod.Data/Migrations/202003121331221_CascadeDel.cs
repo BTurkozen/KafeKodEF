525d69f4b263f57a6d7accfad13864478d3d092b
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2020-03-12 16:32:12 +0300

    Migration eklendi, Database güncellendi.

---
+++ KafeKod.Data/Migrations/202003121331221_CascadeDel.cs
@@ -0,0 +1,20 @@
+﻿namespace KafeKod.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CascadeDel : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.SiparisDetay", "UrunId", "dbo.Urunler");
+            AddForeignKey("dbo.SiparisDetay", "UrunId", "dbo.Urunler", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.SiparisDetay", "UrunId", "dbo.Urunler");
+            AddForeignKey("dbo.SiparisDetay", "UrunId", "dbo.Urunler", "Id", cascadeDelete: true);
+        }
+    }
+}
